3dc7720477284f6c60a239c314a22418a5bb1c22
przemo2174 <przemo217@hotmail.com>
2016-02-10 16:14:36 +0100

    better map

--- ServerApp/phpAPI/maps.php
+++ ServerApp/phpAPI/maps.php
@@ -1,74 +1,64 @@
+<?php
+require_once('json.php');
+$host = "host=127.0.0.1";
+$port = "port=5432";
+$dbname = "dbname=baza_projekt";
+$credentials = "user=sdawidow password=notifications";
+
+
+$db = pg_connect("$host $port $dbname $credentials");
+if(!$db)
+    echo 'Error';
+else
+    echo 'Success';
+
+$date = date('Y-m-d H:i:s');
+
+
+$command = "SELECT * FROM notifications;";
+$ret = pg_query($db, $command);
+$notifications = array();
+
+while($row = pg_fetch_row($ret))
+{
+    $dataArray = array();
+    /*echo "id = ". $row[0] . "<br/>";
+    echo "date = ". $row[1] ."<br/>";
+    echo "name = ". $row[2] ."<br/>";
+    echo "surname = ". $row[3] ."<br/>";
+    echo "notification = ". $row[4] ."<br/>";
+    echo "latitude = ". $row[5] ."<br/>";
+    echo "longitude = ". $row[6] ."<br/>";*/
+    $dataArray['id'] = $row[0];
+    $dataArray['date'] = $row[1];
+    $dataArray['name'] = $row[2];
+    $dataArray['surname'] = $row[3];
+    $dataArray['notification'] = $row[4];
+    $dataArray['latitude'] = $row[5];
+    $dataArray['longitude'] = $row[6];
+    array_push($notifications, $dataArray);
+    $lat = $row[5];
+    $lon = $row[6];
+}
+$json = new Services_JSON();
+pg_close($db);
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
-    <script src="http://maps.googleapis.com/maps/api/js"></script>
+    <script src="http://maps.googleapis.com/maps/api/js?sensor=false"></script>
+    <script src="https://code.jquery.com/jquery-2.2.0.min.js"></script>
+    <link rel="stylesheet" href="style.css"/>
 </head>
 <body>
-<?php
-
-    $host = "host=127.0.0.1";
-    $port = "port=5432";
-    $dbname = "dbname=baza_projekt";
-    $credentials = "user=sdawidow password=notifications";
-
-
-    $db = pg_connect("$host $port $dbname $credentials");
-    if(!$db)
-        echo 'Error';
-    else
-        echo 'Success';
-
-    $date = date('Y-m-d H:i:s');
-
-
-    $command = "SELECT * FROM notifications;";
-    $ret = pg_query($db, $command);
-    $notifications = array();
-
-    while($row = pg_fetch_row($ret))
-    {
-        $dataArray = array();
-        echo "id = ". $row[0] . "<br/>";
-        echo "date = ". $row[1] ."<br/>";
-        echo "name = ". $row[2] ."<br/>";
-        echo "surname = ". $row[3] ."<br/>";
-        echo "notification = ". $row[4] ."<br/>";
-        echo "latitude = ". $row[5] ."<br/>";
-        echo "longitude = ". $row[6] ."<br/>";
-        $dataArray['id'] = $row[0];
-        $dataArray['date'] = $row[1];
-        $dataArray['name'] = $row[2];
-        $dataArray['surname'] = $row[3];
-        $dataArray['notification'] = $row[4];
-        $dataArray['latitude'] = $row[5];
-        $dataArray['longitude'] = $row[6];
-        array_push($notifications, $dataArray);
-        $lat = $row[5];
-        $lon = $row[6];
-    }
-    echo "Operation done sucessfully";
-    pg_close($db);
-?>
 <div id="notificationsInfo"></div>
-<div id="googleMap" style="width:500px;height:380px;"></div>
+<div id="data" style="width:150px;"></div>
+<div id="googleMap" style="width:500px; height:380px; float: right;"></div>
 <script>
-    var notificationsArray = <?php echo json_encode($notifications); ?>;
 
-    var notificationsHtml = document.getElementById("notificationsInfo");
-    for(i = 0; i < notificationsArray.length; i++)
-    {
-        
-    }
-
-    function initialize()
-    {
-        var mapProp = {
-            center:new google.maps.LatLng(51.508742,-0.120850),
-            zoom:5,
-            mapTypeId:google.maps.MapTypeId.ROADMAP
-        };
-        var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
-    }
+    var map;
+    var markers = [];
 
     function initMap()
     {
@@ -79,17 +69,51 @@
             mapTypeId: google.maps.MapTypeId.ROADMAP
         };
 
-        var map = new google.maps.Map(document.getElementById('googleMap'), mapPro);
+        map = new google.maps.Map(document.getElementById('googleMap'), mapPro);
 
+    }
+
+    function AddMarker(location)
+    {
         var marker = new google.maps.Marker({
-            position: myLatLng,
+            position: location,
             map: map,
             title: 'Hello World!'
         });
+        markers.push(marker);
     }
 
-    google.maps.event.addDomListener(window, 'load', initMap);
+    function CreateNotificationDiv(element)
+    {
+        var text = 'Name: ' + element['name']  + '\n' +
+            'Surname: ' + element['surname'] + '\n' +
+            'Notification: ' + element['notification'] + '\n' +
+            'Latitude: ' + element['latitude'] + '\n' +
+            'Longitude: ' + element['longitude'];
+
+        $('<div/>', {"class" : 'record'}).text(text).click(function()
+        {
+            AddMarker({lat: parseFloat(element['latitude']), lng: parseFloat(element['longitude'])});
+        }).appendTo("#data");
+
+    }
+
+    $(document).ready(function()
+    {
+        var notificationsHtml = document.getElementById("notificationsInfo");
+        var notificationsArray = <?php echo $json->encode($notifications); ?>;
+        //for(var i = 0; i < notificationsArray.length; i++)
+        //{
+        //    notificationsHtml.innerHTML = JSON.stringify(notificationsArray);
+        //}
+
+
+        google.maps.event.addDomListener(window, 'load', initMap);
+        notificationsArray.forEach(CreateNotificationDiv);
+    });
+
+
+
 </script>
 </body>
-
 </html>
